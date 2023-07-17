abe80103da2a4e60150b3dd27fcf8ca820eeb0b0
NotIntense <mbro8210@gmail.com>
2023-07-17 14:07:28 -0400

    Added Directorys

--- Nebuli/API/Features/Paths.cs
+++ Nebuli/API/Features/Paths.cs
@@ -10,6 +10,8 @@ public static class Paths
     public static DirectoryInfo PluginsDirectory { get; private set; }
 
     public static DirectoryInfo DependenciesDirectory { get; private set; }
+
+    public static DirectoryInfo PluginConfigDirectory { get; private set; }
     
     public static FileInfo Configs { get; private set; }
     
@@ -18,5 +20,9 @@ public static class Paths
     public static void LoadPaths()
     {
         MainDirectory = Directory.CreateDirectory(Path.Combine(NwPaths.SecretLab, "Nebuli"));
+        PluginsDirectory = Directory.CreateDirectory(Path.Combine(MainDirectory.FullName, "Plugins"));
+        DependenciesDirectory = Directory.CreateDirectory(Path.Combine(MainDirectory.FullName, "Dependencies"));
+        PluginConfigDirectory = Directory.CreateDirectory(Path.Combine(PluginsDirectory.FullName, "PluginConfigs"));
     }
+
 }
