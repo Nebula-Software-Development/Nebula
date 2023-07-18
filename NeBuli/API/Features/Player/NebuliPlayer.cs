0ee64ea95948a2d676a93e46b85352f1e41a8a7f
NotIntense <mbro8210@gmail.com>
2023-07-17 22:29:17 -0400

    rawr

--- NeBuli/API/Features/Player/NebuliPlayer.cs
+++ NeBuli/API/Features/Player/NebuliPlayer.cs
@@ -34,13 +34,24 @@ public class NebuliPlayer
     public static List<NebuliPlayer> List => Collection.ToList();
    
     public static int PlayerCount => Dictionary.Count;
-    
+    /// <summary>
+    /// The player's ReferenceHub.
+    /// </summary>
     public ReferenceHub ReferenceHub { get; }
-    
+
+    /// <summary>
+    /// The player's GameObject.
+    /// </summary>
     public GameObject GameObject { get; }
-    
+
+    /// <summary>
+    /// The player' Transform.
+    /// </summary>
     public Transform Transform { get; }
-    
+
+    /// <summary>
+    /// The players RawUserId.
+    /// </summary>
     public string RawUserId { get; private set; }
 
     /// <summary>
@@ -263,13 +274,13 @@ public class NebuliPlayer
         get => ReferenceHub.serverRoles.Permissions;
         set => ReferenceHub.serverRoles.Permissions = value;
     }
-    
+
     /// <summary>
     /// Trys to get a <see cref="NebuliPlayer"/> with the provided Referencehub.
     /// </summary>
     /// <param name="hub">The players referencehub.</param>
     /// <param name="player">The player that will be returned, if found.</param>
-    /// <returns>The <see cref="NebuliPlayer"/>, otherwise null.</returns>
+    /// <returns>True if found, otherwise false.</returns>
     public static bool TryGet(ReferenceHub hub, out NebuliPlayer player)
     {
         if (hub is not null && Dictionary.TryGetValue(hub, out NebuliPlayer value))
@@ -287,7 +298,7 @@ public class NebuliPlayer
     /// </summary>
     /// <param name="gameobject">The players GameObject.</param>
     /// <param name="player">The player that will be returned, if found.</param>
-    /// <returns>The <see cref="NebuliPlayer"/>, otherwise null.</returns>
+    /// <returns>True if found, otherwise false.</returns>
     public static bool TryGet(GameObject gameobject, out NebuliPlayer player)
     {
         foreach (var ply in Collection)
@@ -302,11 +313,28 @@ public class NebuliPlayer
         player = null;
         return false;
     }
-    
+    /// <summary>
+    /// Trys to get a <see cref="NebuliPlayer"/> with the provided MonoBehavior component.
+    /// </summary>
+    /// <param name="component">The players Monobehavior component.</param>
+    /// <param name="player">The player that will be returned, if found.</param>
+    /// <returns>True if found, otherwise false.</returns>
     public static bool TryGet(MonoBehaviour component, out NebuliPlayer player) => TryGet(component.gameObject, out player);
 
+    /// <summary>
+    /// Trys to get a <see cref="NebuliPlayer"/> with the provided <see cref="NetworkIdentity"/> component.
+    /// </summary>
+    /// <param name="identity">The players <see cref="NetworkIdentity"/> component.</param>
+    /// <param name="player">The player that will be returned, if found.</param>
+    /// <returns>True if found, otherwise false.</returns>
     public static bool TryGet(NetworkIdentity identity, out NebuliPlayer player) => TryGet(identity.gameObject, out player);
-    
+
+    /// <summary>
+    /// Tries to get a NebuliPlayer instance based on their PlayerId.
+    /// </summary>
+    /// <param name="id">The PlayerId of the player.</param>
+    /// <param name="player">When this method returns, contains the NebuliPlayer instance if found; otherwise, null.</param>
+    /// <returns>True if the NebuliPlayer instance was found; otherwise, false.</returns>
     public static bool TryGet(int id, out NebuliPlayer player)
     {
         foreach (ReferenceHub hub in ReferenceHub.AllHubs)
@@ -318,70 +346,264 @@ public class NebuliPlayer
         player = null;
         return false;
     }
-    
+
+    /// <summary>
+    /// Tries to get a NebuliPlayer instance based on their NetworkId.
+    /// </summary>
+    /// <param name="netId">The NetworkId of the player.</param>
+    /// <param name="player">When this method returns, contains the NebuliPlayer instance if found; otherwise, null.</param>
+    /// <returns>True if the NebuliPlayer instance was found; otherwise, false.</returns>
     public static bool TryGet(uint netId, out NebuliPlayer player)
     {
         if (ReferenceHub.TryGetHubNetID(netId, out ReferenceHub hub))
             return TryGet(hub, out player);
-        
+
         player = null;
         return false;
     }
-    
-    public static NebuliPlayer Get(ReferenceHub hub) => TryGet(hub, out NebuliPlayer player) ? player : null;
-   
-    public static NebuliPlayer Get(GameObject go) => TryGet(go, out NebuliPlayer player) ? player : null;
-   
-    public static NebuliPlayer Get(MonoBehaviour component) => TryGet(component, out NebuliPlayer player) ? player : null;
-  
-    public static NebuliPlayer Get(NetworkIdentity identity) => TryGet(identity, out NebuliPlayer player) ? player : null;
-   
-    public static NebuliPlayer Get(int id) => TryGet(id, out NebuliPlayer player) ? player : null;
-    
-    public static NebuliPlayer Get(uint netId) => TryGet(netId, out NebuliPlayer player) ? player : null;
-    
-    public void Kill(string text) => Kill(new CustomReasonDamageHandler(text, float.MaxValue));
 
-    public void Kill(DamageHandlerBase damageHandlerBase) => ReferenceHub.playerStats.KillPlayer(damageHandlerBase);
+    /// <summary>
+    /// Gets a NebuliPlayer instance based on a ReferenceHub.
+    /// </summary>
+    /// <param name="hub">The ReferenceHub of the player.</param>
+    /// <returns>The NebuliPlayer instance if found; otherwise, null.</returns>
+    public static NebuliPlayer Get(ReferenceHub hub)
+    {
+        return TryGet(hub, out NebuliPlayer player) ? player : null;
+    }
 
-    public void Damage(float amount, string reason = "") => ReferenceHub.playerStats.DealDamage(new CustomReasonDamageHandler(reason, amount));
-    
-    public void Damage(DamageHandlerBase damageHandlerBase) => ReferenceHub.playerStats.DealDamage(damageHandlerBase);
-    
-    public bool TryGetEffect(string effectName, out StatusEffectBase effect) => ReferenceHub.playerEffectsController.TryGetEffect(effectName, out effect);
-    
-    public bool TryGetEffect<T>(out T effect) 
-        where T : StatusEffectBase => ReferenceHub.playerEffectsController.TryGetEffect(out effect);
-    
-    public T EnableEffect<T>(float duration = 0f, bool addDuration = false) 
-        where T : StatusEffectBase =>
-        ReferenceHub.playerEffectsController.EnableEffect<T>(duration, addDuration);
-    
-    public T DisableEffect<T>() 
-        where T : StatusEffectBase => ReferenceHub.playerEffectsController.DisableEffect<T>();
+    /// <summary>
+    /// Gets a NebuliPlayer instance based on a GameObject.
+    /// </summary>
+    /// <param name="go">The GameObject of the player.</param>
+    /// <returns>The NebuliPlayer instance if found; otherwise, null.</returns>
+    public static NebuliPlayer Get(GameObject go)
+    {
+        return TryGet(go, out NebuliPlayer player) ? player : null;
+    }
 
-    public void DisableAllEffects() => ReferenceHub.playerEffectsController.DisableAllEffects();
-    
-    public void ShowTag(bool global = false) => ReferenceHub.characterClassManager.UserCode_CmdRequestShowTag__Boolean(global);
-    
-    public void HideTag() => ReferenceHub.characterClassManager.UserCode_CmdRequestHideTag();
-    
-    public void ShowHint(string content, int time = 5) => ShowHint(new TextHint(content, new HintParameter[] { new StringHintParameter(string.Empty) }, null, 2));
-    
-    public void ShowHint(Hint hint) => ReferenceHub.hints.Show(hint);
-    
-    public void SetRole(RoleTypeId role, RoleChangeReason reason = RoleChangeReason.RemoteAdmin, RoleSpawnFlags flags = RoleSpawnFlags.All) => ReferenceHub.roleManager.ServerSetRole(role, reason, flags);
-    
-    public T AddComponent<T>() 
-        where T : MonoBehaviour => GameObject.AddComponent<T>();
-   
-    public T GetComponent<T>()
-        where T : MonoBehaviour => GameObject.GetComponent<T>();
-    
-    public bool Kick(string reason) => BanPlayer.KickUser(ReferenceHub, reason);
+    /// <summary>
+    /// Gets a NebuliPlayer instance based on a MonoBehaviour component.
+    /// </summary>
+    /// <param name="component">The MonoBehaviour component of the player.</param>
+    /// <returns>The NebuliPlayer instance if found; otherwise, null.</returns>
+    public static NebuliPlayer Get(MonoBehaviour component)
+    {
+        return TryGet(component, out NebuliPlayer player) ? player : null;
+    }
 
-    public bool Ban(string reason, long duration) => BanPlayer.BanUser(ReferenceHub, reason, duration);
-    
+    /// <summary>
+    /// Gets a NebuliPlayer instance based on a NetworkIdentity.
+    /// </summary>
+    /// <param name="identity">The NetworkIdentity of the player.</param>
+    /// <returns>The NebuliPlayer instance if found; otherwise, null.</returns>
+    public static NebuliPlayer Get(NetworkIdentity identity)
+    {
+        return TryGet(identity, out NebuliPlayer player) ? player : null;
+    }
+
+    /// <summary>
+    /// Gets a NebuliPlayer instance based on their PlayerId.
+    /// </summary>
+    /// <param name="id">The PlayerId of the player.</param>
+    /// <returns>The NebuliPlayer instance if found; otherwise, null.</returns>
+    public static NebuliPlayer Get(int id)
+    {
+        return TryGet(id, out NebuliPlayer player) ? player : null;
+    }
+
+    /// <summary>
+    /// Gets a NebuliPlayer instance based on their NetworkId.
+    /// </summary>
+    /// <param name="netId">The NetworkId of the player.</param>
+    /// <returns>The NebuliPlayer instance if found; otherwise, null.</returns>
+    public static NebuliPlayer Get(uint netId)
+    {
+        return TryGet(netId, out NebuliPlayer player) ? player : null;
+    }
+
+    /// <summary>
+    /// Kills the player with a custom reason.
+    /// </summary>
+    /// <param name="text">The reason for the kill.</param>
+    public void Kill(string text)
+    {
+        Kill(new CustomReasonDamageHandler(text, float.MaxValue));
+    }
+
+    /// <summary>
+    /// Kills the player with a custom damage handler.
+    /// </summary>
+    /// <param name="damageHandlerBase">The custom damage handler for the kill.</param>
+    public void Kill(DamageHandlerBase damageHandlerBase)
+    {
+        ReferenceHub.playerStats.KillPlayer(damageHandlerBase);
+    }
+
+    /// <summary>
+    /// Deals damage to the player with a custom amount and reason.
+    /// </summary>
+    /// <param name="amount">The amount of damage.</param>
+    /// <param name="reason">The reason for the damage. (Optional)</param>
+    public void Damage(float amount, string reason = "")
+    {
+        ReferenceHub.playerStats.DealDamage(new CustomReasonDamageHandler(reason, amount));
+    }
+
+    /// <summary>
+    /// Deals damage to the player with a custom damage handler.
+    /// </summary>
+    /// <param name="damageHandlerBase">The custom damage handler for the damage.</param>
+    public void Damage(DamageHandlerBase damageHandlerBase)
+    {
+        ReferenceHub.playerStats.DealDamage(damageHandlerBase);
+    }
+
+    /// <summary>
+    /// Tries to get a specific status effect on the player based on its name.
+    /// </summary>
+    /// <param name="effectName">The name of the status effect.</param>
+    /// <param name="effect">When this method returns, contains the status effect instance if found; otherwise, null.</param>
+    /// <returns>True if the status effect was found; otherwise, false.</returns>
+    public bool TryGetEffect(string effectName, out StatusEffectBase effect)
+    {
+        return ReferenceHub.playerEffectsController.TryGetEffect(effectName, out effect);
+    }
+
+    /// <summary>
+    /// Tries to get a specific status effect on the player based on its type.
+    /// </summary>
+    /// <typeparam name="T">The type of the status effect.</typeparam>
+    /// <param name="effect">When this method returns, contains the status effect instance if found; otherwise, null.</param>
+    /// <returns>True if the status effect was found; otherwise, false.</returns>
+    public bool TryGetEffect<T>(out T effect) where T : StatusEffectBase
+    {
+        return ReferenceHub.playerEffectsController.TryGetEffect(out effect);
+    }
+
+    /// <summary>
+    /// Enables a specific status effect on the player.
+    /// </summary>
+    /// <typeparam name="T">The type of the status effect.</typeparam>
+    /// <param name="duration">The duration of the status effect. (Optional)</param>
+    /// <param name="addDuration">Whether to add the duration to the existing effect if already active. (Optional)</param>
+    /// <returns>The enabled status effect instance.</returns>
+    public T EnableEffect<T>(float duration = 0f, bool addDuration = false) where T : StatusEffectBase
+    {
+        return ReferenceHub.playerEffectsController.EnableEffect<T>(duration, addDuration);
+    }
+
+    /// <summary>
+    /// Disables a specific status effect on the player.
+    /// </summary>
+    /// <typeparam name="T">The type of the status effect.</typeparam>
+    /// <returns>The disabled status effect instance.</returns>
+    public T DisableEffect<T>() where T : StatusEffectBase
+    {
+        return ReferenceHub.playerEffectsController.DisableEffect<T>();
+    }
+
+    /// <summary>
+    /// Disables all status effects on the player.
+    /// </summary>
+    public void DisableAllEffects()
+    {
+        ReferenceHub.playerEffectsController.DisableAllEffects();
+    }
+
+    /// <summary>
+    /// Shows the player's name tag.
+    /// </summary>
+    /// <param name="global">Whether to show the name tag to all players (SCP-079). (Optional)</param>
+    public void ShowTag(bool global = false)
+    {
+        ReferenceHub.characterClassManager.UserCode_CmdRequestShowTag__Boolean(global);
+    }
+
+    /// <summary>
+    /// Hides the player's name tag.
+    /// </summary>
+    public void HideTag()
+    {
+        ReferenceHub.characterClassManager.UserCode_CmdRequestHideTag();
+    }
+
+    /// <summary>
+    /// Shows a hint to the player with the specified content and duration.
+    /// </summary>
+    /// <param name="content">The content of the hint.</param>
+    /// <param name="time">The duration of the hint in seconds. (Optional)</param>
+    public void ShowHint(string content, int time = 5)
+    {
+        ShowHint(new TextHint(content, new HintParameter[] { new StringHintParameter(string.Empty) }, null, 2));
+    }
+
+    /// <summary>
+    /// Shows a custom hint to the player.
+    /// </summary>
+    /// <param name="hint">The custom hint to show.</param>
+    public void ShowHint(Hint hint)
+    {
+        ReferenceHub.hints.Show(hint);
+    }
+
+    /// <summary>
+    /// Sets the role of the player.
+    /// </summary>
+    /// <param name="role">The role to set.</param>
+    /// <param name="reason">The reason for the role change. (Optional)</param>
+    /// <param name="flags">The flags for the role spawn. (Optional)</param>
+    public void SetRole(RoleTypeId role, RoleChangeReason reason = RoleChangeReason.RemoteAdmin, RoleSpawnFlags flags = RoleSpawnFlags.All)
+    {
+        ReferenceHub.roleManager.ServerSetRole(role, reason, flags);
+    }
+
+    /// <summary>
+    /// Adds a component of the specified type to the player's GameObject.
+    /// </summary>
+    /// <typeparam name="T">The type of the component to add.</typeparam>
+    /// <returns>The added component instance.</returns>
+    public T AddComponent<T>() where T : MonoBehaviour
+    {
+        return GameObject.AddComponent<T>();
+    }
+
+    /// <summary>
+    /// Gets the component of the specified type from the player's GameObject.
+    /// </summary>
+    /// <typeparam name="T">The type of the component to get.</typeparam>
+    /// <returns>The component instance if found; otherwise, null.</returns>
+    public T GetComponent<T>() where T : MonoBehaviour
+    {
+        return GameObject.GetComponent<T>();
+    }
+
+    /// <summary>
+    /// Kicks the player from the server with the specified reason.
+    /// </summary>
+    /// <param name="reason">The reason for the kick.</param>
+    /// <returns>True if the kick was successful; otherwise, false.</returns>
+    public bool Kick(string reason)
+    {
+        return BanPlayer.KickUser(ReferenceHub, reason);
+    }
+
+    /// <summary>
+    /// Bans the player from the server with the specified reason and duration.
+    /// </summary>
+    /// <param name="reason">The reason for the ban.</param>
+    /// <param name="duration">The duration of the ban in seconds.</param>
+    /// <returns>True if the ban was successful; otherwise, false.</returns>
+    public bool Ban(string reason, long duration)
+    {
+        return BanPlayer.BanUser(ReferenceHub, reason, duration);
+    }
+
+    /// <summary>
+    /// Mutes the player's voice chat.
+    /// </summary>
+    /// <param name="isIntercom">Whether to mute the player globally or only on intercom. (Optional)</param>
+    /// <param name="isTemporary">Whether to apply a temporary mute or a regular global mute. (Optional)</param>
     public void Mute(bool isIntercom = false, bool isTemporary = true)
     {
         if (isTemporary)
@@ -389,10 +611,14 @@ public class NebuliPlayer
             VoiceChatMutes.SetFlags(ReferenceHub, VcMuteFlags.GlobalRegular);
             return;
         }
-        
+
         VoiceChatMutes.IssueLocalMute(UserId, isIntercom);
     }
-    
+
+    /// <summary>
+    /// Unmutes the player's voice chat.
+    /// </summary>
+    /// <param name="removeMute">Whether to remove the player's local mute or the global mute. (Optional)</param>
     public void UnMute(bool removeMute = true)
     {
         if (removeMute)
@@ -400,16 +626,42 @@ public class NebuliPlayer
             VoiceChatMutes.RevokeLocalMute(UserId);
             return;
         }
-        
+
         VoiceChatMutes.SetFlags(ReferenceHub, VcMuteFlags.None);
     }
-    
-    public void SendHitMarker(float size = 2.55f) => Hitmarker.SendHitmarker(ReferenceHub, size);
-    
-    public bool HasPermission(PlayerPermissions permission) => PermissionsHandler.IsPermitted(Permissions, permission);
-    
-    public bool HasPermissions(PlayerPermissions[] permissions) => PermissionsHandler.IsPermitted(Permissions, permissions);
 
+    /// <summary>
+    /// Sends a hit marker effect to the player.
+    /// </summary>
+    /// <param name="size">The size of the hit marker. (Optional)</param>
+    public void SendHitMarker(float size = 2.55f)
+    {
+        Hitmarker.SendHitmarker(ReferenceHub, size);
+    }
+
+    /// <summary>
+    /// Checks if the player has the specified permission.
+    /// </summary>
+    /// <param name="permission">The permission to check.</param>
+    /// <returns>True if the player has the permission; otherwise, false.</returns>
+    public bool HasPermission(PlayerPermissions permission)
+    {
+        return PermissionsHandler.IsPermitted(Permissions, permission);
+    }
+
+    /// <summary>
+    /// Checks if the player has any of the specified permissions.
+    /// </summary>
+    /// <param name="permissions">The array of permissions to check.</param>
+    /// <returns>True if the player has any of the permissions; otherwise, false.</returns>
+    public bool HasPermissions(PlayerPermissions[] permissions)
+    {
+        return PermissionsHandler.IsPermitted(Permissions, permissions);
+    }
+
+    /// <summary>
+    /// Parses the UserId to extract the RawUserId without the discriminator.
+    /// </summary>
     private void Create()
     {
         int index = UserId.LastIndexOf('@');
@@ -419,7 +671,8 @@ public class NebuliPlayer
             RawUserId = UserId;
             return;
         }
-        
+
         RawUserId = UserId.Substring(0, index);
     }
+
 }
