8790df137ffb14b974bb3f09d14dec9865618857
NotIntense <mbro8210@gmail.com>
2023-07-20 18:02:23 -0400

    rawr

---
+++ NeBuli/Events/EventArguments/Server/WarheadDetonatedEventArgs.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Nebuli.Events.EventArguments.Server
+{
+    public class WarheadDetonatedEventArgs : EventArgs
+    {
+        public WarheadDetonatedEventArgs()
+        {
+
+        }
+    }
+}
--- NeBuli/Events/Handlers/ServerHandler.cs
+++ NeBuli/Events/Handlers/ServerHandler.cs
@@ -6,6 +6,10 @@ namespace Nebuli.Events.Handlers
     {
         public static EventManager.CustomEventHandler<RoundStartedEvent> RoundStart;
 
+        public static EventManager.CustomEventHandler<WarheadDetonatedEventArgs> WarheadDetonated;
+
         internal static void OnRoundStart() => RoundStart.CallEvent(null);
+
+        internal static void OnWarheadDetonated(WarheadDetonatedEventArgs ev) => WarheadDetonated.CallEvent(ev);
     }
 }
