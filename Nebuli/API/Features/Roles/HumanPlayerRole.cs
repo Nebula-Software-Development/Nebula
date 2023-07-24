5ea88253a450081922f5c70e210e9f549f743182
NotIntense <mbro8210@gmail.com>
2023-07-24 15:57:50 -0400

    upboot

--- Nebuli/API/Features/Roles/HumanPlayerRole.cs
+++ Nebuli/API/Features/Roles/HumanPlayerRole.cs
@@ -6,12 +6,11 @@ using Respawning;
 
 namespace Nebuli.API.Features.Roles;
 
-public class HumanPlayerRole : Role
+public class HumanPlayerRole : FpcRoleBase
 {
     internal HumanPlayerRole(HumanRole role) : base(role)
     {
         Base = role;
-        Ragdoll = Ragdoll.Get(Base.Ragdoll);
     }
 
     /// <summary>
@@ -19,35 +18,6 @@ public class HumanPlayerRole : Role
     /// </summary>
     public new HumanRole Base { get; }
 
-    /// <summary>
-    /// Gets the roles <see cref="Player.Ragdoll"/>.
-    /// </summary>
-    public Ragdoll Ragdoll { get; }
-
-    /// <summary>
-    /// Gets the roles <see cref="VoiceModuleBase"/>.
-    /// </summary>
-    public VoiceModuleBase VoiceModule => Base.VoiceModule;
-
-    /// <summary>
-    /// Gets if the role is AFK.
-    /// </summary>
-    public bool IsAFK => Base.IsAFK;
-
-    /// <summary>
-    /// Re-shows the players start screen.
-    /// </summary>
-    public void ShowStartScreen() => Base.ShowStartScreen();
-
-    /// <summary>
-    /// Gets the roles <see cref="ISpawnpointHandler"/>.
-    /// </summary>
-    public ISpawnpointHandler SpawnpointHandler => Base.SpawnpointHandler;
-
-    /// <summary>
-    /// Gets the roles Spectator module.
-    /// </summary>
-    public PlayerRoles.Spectating.SpectatableModuleBase SpectatableModuleBase => Base.SpectatorModule;
 
     /// <summary>
     /// Gets the roles RoleTypeId.
