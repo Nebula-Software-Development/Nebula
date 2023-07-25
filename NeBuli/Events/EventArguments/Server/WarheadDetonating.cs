b096c60450d85603f73e148deeb54befcf9df217
NotIntense <mbro8210@gmail.com>
2023-07-25 10:30:59 -0400

    warhead event change

---
+++ NeBuli/Events/EventArguments/Server/WarheadDetonating.cs
@@ -0,0 +1,17 @@
+﻿using Nebuli.API.Features.Player;
+using System;
+
+namespace Nebuli.Events.EventArguments.Server
+{
+    public class WarheadDetonating : EventArgs, IPlayerEvent, ICancellableEvent
+    {
+        public WarheadDetonating(ReferenceHub player)
+        {
+            Player = NebuliPlayer.Get(player);
+            IsCancelled = false;
+        }
+
+        public NebuliPlayer Player { get; }
+        public bool IsCancelled { get; set; }
+    }
+}
