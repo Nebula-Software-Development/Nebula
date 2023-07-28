beab6c3498c4d2054843eb679c02c359f7e207eb
xNexus-ACS <83370388+xNexus-ACS@users.noreply.github.com>
2023-07-28 23:56:55 +0200

    Tesla Patch

--- NeBuli/Events/Patches/Player/TriggeringTesla.cs
+++ NeBuli/Events/Patches/Player/TriggeringTesla.cs
@@ -1,39 +1,52 @@
-﻿using HarmonyLib;
+﻿using System.Collections.Generic;
+using System.Reflection.Emit;
+using HarmonyLib;
+using Nebuli.API.Features.Map;
+using Nebuli.Events.EventArguments.Player;
 using Nebuli.Events.Handlers;
 using NorthwoodLib.Pools;
-using System.Collections.Generic;
-using System.Reflection.Emit;
 using static HarmonyLib.AccessTools;
 
 namespace Nebuli.Events.Patches.Player;
 
-//[HarmonyPatch(typeof(TeslaGateController), nameof(TeslaGateController.FixedUpdate))]
+[HarmonyPatch(typeof(TeslaGateController), nameof(TeslaGateController.FixedUpdate))]
 public class TriggeringTesla
 {
     [HarmonyTranspiler]
-    private static IEnumerable<CodeInstruction> OnTrigger(IEnumerable<CodeInstruction> instructions, ILGenerator generator)
+    private static IEnumerable<CodeInstruction> OnTriggerTesla(IEnumerable<CodeInstruction> instructions, ILGenerator generator)
     {
         List<CodeInstruction> newInstructions = EventManager.CheckPatchInstructions<TriggeringTesla>(101, instructions);
 
-        Label retLabel = generator.DefineLabel();
-
-        int index = newInstructions.FindIndex(instruction => instruction.opcode == OpCodes.Ret);
+        int index = newInstructions.FindIndex(instruction => instruction.Calls(AccessTools.PropertyGetter(typeof(ReferenceHub), nameof(ReferenceHub.AllHubs))));
+        
+        newInstructions.RemoveRange(index, newInstructions.FindIndex(i => i.opcode == OpCodes.Endfinally) + 1 - index);
 
         newInstructions.InsertRange(index, new CodeInstruction[]
         {
-            new CodeInstruction(OpCodes.Ldloc_1),
-            new CodeInstruction(OpCodes.Ldloca_S, 2),
-            new CodeInstruction(OpCodes.Ldloca_S, 3),
-            new CodeInstruction(OpCodes.Ldloca_S, 5),
-            new CodeInstruction(OpCodes.Call, Method(typeof(PlayerHandlers), nameof(PlayerHandlers.OnTriggerTesla))),
-            new CodeInstruction(OpCodes.Pop),
+            new(OpCodes.Ldloc_1),
+            new(OpCodes.Ldloca_S, 2),
+            new(OpCodes.Ldloca_S, 3),
+            new(OpCodes.Call, Method(typeof(TriggeringTesla), nameof(ProcessEvent), new[] { typeof(TeslaGate), typeof(bool).MakeByRefType(), typeof(bool).MakeByRefType() })),
         });
-
-        newInstructions[newInstructions.Count - 1].labels.Add(retLabel);
-
+        
         foreach (CodeInstruction instruction in newInstructions)
             yield return instruction;
 
         ListPool<CodeInstruction>.Shared.Return(newInstructions);
+    }
+
+    private static void ProcessEvent(TeslaGate teslaGate, ref bool inIdleRange, ref bool isTriggerable)
+    {
+        var args = new PlayerTriggeringTesla(NebuliTeslaGate.Get(teslaGate).NearestPlayer.ReferenceHub, teslaGate, inIdleRange, isTriggerable);
+        PlayerHandlers.OnTriggerTesla(args);
+
+        if (args.IsCancelled)
+            return;
+        
+        if (args.IsTriggerable && !isTriggerable)
+            isTriggerable = args.IsTriggerable;
+
+        if (args.IsInIdleRange && !inIdleRange)
+            inIdleRange = args.IsInIdleRange;
     }
 }
