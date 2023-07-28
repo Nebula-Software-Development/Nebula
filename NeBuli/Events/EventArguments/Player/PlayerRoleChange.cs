b53bea2e8917c18da5db56cb0b72401d5ffc05bc
xNexus-ACS <83370388+xNexus-ACS@users.noreply.github.com>
2023-07-28 23:19:50 +0200

    RoleChange Event Update

--- NeBuli/Events/EventArguments/Player/PlayerRoleChange.cs
+++ NeBuli/Events/EventArguments/Player/PlayerRoleChange.cs
@@ -4,14 +4,15 @@ using System;
 
 namespace Nebuli.Events.EventArguments.Player;
 
-public class PlayerRoleChange : EventArgs, IPlayerEvent
+public class PlayerRoleChange : EventArgs, IPlayerEvent, ICancellableEvent
 {
-    public PlayerRoleChange(ReferenceHub ply, RoleTypeId newRole, RoleChangeReason roleChangeReason, RoleSpawnFlags roleSpawnFlags = RoleSpawnFlags.All)
+    public PlayerRoleChange(ReferenceHub ply, RoleTypeId newRole, RoleChangeReason roleChangeReason, RoleSpawnFlags roleSpawnFlags)
     {
         Player = NebuliPlayer.Get(ply);
         NewRole = newRole;
         Reason = roleChangeReason;
         SpawnFlags = roleSpawnFlags;
+        IsCancelled = false;
     }
 
     /// <summary>
@@ -19,9 +20,11 @@ public class PlayerRoleChange : EventArgs, IPlayerEvent
     /// </summary>
     public NebuliPlayer Player { get; }
 
-    public RoleTypeId NewRole { get; }
+    public RoleTypeId NewRole { get; set; }
 
-    public RoleChangeReason Reason { get; }
+    public RoleChangeReason Reason { get; set; }
 
-    public RoleSpawnFlags SpawnFlags { get; }
+    public RoleSpawnFlags SpawnFlags { get; set; }
+
+    public bool IsCancelled { get; set; }
 }
