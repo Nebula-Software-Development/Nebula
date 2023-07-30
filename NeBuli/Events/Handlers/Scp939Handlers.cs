d421767f3030825808fd04ed75395b54c4656206
xNexus-ACS <83370388+xNexus-ACS@users.noreply.github.com>
2023-07-30 22:32:52 +0200

    Scp939 Handlers

---
+++ NeBuli/Events/Handlers/Scp939Handlers.cs
@@ -0,0 +1,50 @@
+﻿using Nebuli.Events.EventArguments.SCPs.Scp939;
+
+namespace Nebuli.Events.Handlers;
+
+public static class Scp939Handlers
+{
+    public static EventManager.CustomEventHandler<Scp939PlaceCloud> PlaceCloud;
+    
+    public static EventManager.CustomEventHandler<Scp939CancelCloudPlacement> CancelCloudPlacement;
+    
+    public static EventManager.CustomEventHandler<Scp939PlaceMimicPoint> PlaceMimicPoint;
+    
+    public static EventManager.CustomEventHandler<Scp939RemoveMimicPoint> RemoveMimicPoint;
+    
+    public static EventManager.CustomEventHandler<Scp939PlaySound> PlaySound;
+    
+    public static EventManager.CustomEventHandler<Scp939PlayVoice> PlayVoice;
+    
+    public static EventManager.CustomEventHandler<Scp939SavePlayerVoice> SaveVoice;
+    
+    public static EventManager.CustomEventHandler<Scp939RemoveSavedVoice> RemoveVoice;
+    
+    public static EventManager.CustomEventHandler<Scp939UseLunge> UseLunge;
+    
+    public static EventManager.CustomEventHandler<Scp939ToggleFocus> ToggleFocus;
+    
+    public static EventManager.CustomEventHandler<Scp939Attack> Attack;
+
+    internal static void OnPlaceCloud(Scp939PlaceCloud ev) => PlaceCloud.CallEvent(ev);
+    
+    internal static void OnCancelCloudPlacement(Scp939CancelCloudPlacement ev) => CancelCloudPlacement.CallEvent(ev);
+    
+    internal static void OnPlaceMimicPoint(Scp939PlaceMimicPoint ev) => PlaceMimicPoint.CallEvent(ev);
+    
+    internal static void OnRemoveMimicPoint(Scp939RemoveMimicPoint ev) => RemoveMimicPoint.CallEvent(ev);
+    
+    internal static void OnPlaySound(Scp939PlaySound ev) => PlaySound.CallEvent(ev);
+    
+    internal static void OnPlayVoice(Scp939PlayVoice ev) => PlayVoice.CallEvent(ev);
+    
+    internal static void OnSaveVoice(Scp939SavePlayerVoice ev) => SaveVoice.CallEvent(ev);
+    
+    internal static void OnRemoveVoice(Scp939RemoveSavedVoice ev) => RemoveVoice.CallEvent(ev);
+    
+    internal static void OnUseLunge(Scp939UseLunge ev) => UseLunge.CallEvent(ev);
+    
+    internal static void OnToggleFocus(Scp939ToggleFocus ev) => ToggleFocus.CallEvent(ev);
+    
+    internal static void OnAttack(Scp939Attack ev) => Attack.CallEvent(ev);
+}
