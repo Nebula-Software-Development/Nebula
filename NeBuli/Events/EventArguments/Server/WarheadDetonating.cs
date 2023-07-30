d8487e9d4160c4ba1aac1c12dbd11f1077227c78
NotIntense <mbro8210@gmail.com>
2023-07-30 15:55:00 -0400

    Warhead patch fix

--- NeBuli/Events/EventArguments/Server/WarheadDetonating.cs
+++ NeBuli/Events/EventArguments/Server/WarheadDetonating.cs
@@ -7,11 +7,18 @@ namespace Nebuli.Events.EventArguments.Server
     {
         public WarheadDetonating(ReferenceHub player)
         {
-            Player = NebuliPlayer.Get(player);
+            Player = NebuliPlayer.TryGet(player, out NebuliPlayer ply) ? ply : API.Features.Server.NebuliHost;
             IsCancelled = false;
         }
 
+        /// <summary>
+        /// The player triggering the event, will be the Host player if null.
+        /// </summary>
         public NebuliPlayer Player { get; }
+
+        /// <summary>
+        /// Gets or sets if the event is cancelled.
+        /// </summary>
         public bool IsCancelled { get; set; }
     }
 }
