b13594f33acaa2a57e4233972004eb0c3a450dc4
xNexus-ACS <83370388+xNexus-ACS@users.noreply.github.com>
2023-08-01 14:24:17 +0200

    All Scp173 Event Args finished

--- NeBuli/Events/EventArguments/SCPs/Scp173/Scp173PlaceTantrum.cs
+++ NeBuli/Events/EventArguments/SCPs/Scp173/Scp173PlaceTantrum.cs
@@ -1,21 +1,17 @@
 using System;
 using Nebuli.API.Features.Player;
-using UnityEngine;
 
 namespace Nebuli.Events.EventArguments.SCPs.Scp173;
 
-public class Scp173Blink : EventArgs, IPlayerEvent, ICancellableEvent
+public class Scp173PlaceTantrum : EventArgs, IPlayerEvent, ICancellableEvent
 {
-    public Scp173Blink(ReferenceHub player, Vector3 position)
+    public Scp173PlaceTantrum(ReferenceHub player)
     {
         Player = NebuliPlayer.Get(player);
-        Position = position;
         IsCancelled = false;
     }
     
     public NebuliPlayer Player { get; }
-    
-    public Vector3 Position { get; set; }
 
     public bool IsCancelled { get; set; }
 }
