2d479e5b5f6122d2c5f6a6a600f97da7c98cff4b
NotIntense <mbro8210@gmail.com>
2023-08-08 06:48:27 -0400

    Delete Achievements.cs

--- NeBuli/API/Features/Achievements.cs
+++
@@ -1,9 +0,0 @@
-﻿using Achievements;
-using Nebuli.API.Features.Player;
-
-namespace Nebuli.API.Features;
-
-public static class Achievements
-{
-    public static void GiveClientAchievement(NebuliPlayer player, AchievementName achievement) => AchievementHandlerBase.ServerAchieve(player.ReferenceHub.connectionToClient, achievement);
-}
