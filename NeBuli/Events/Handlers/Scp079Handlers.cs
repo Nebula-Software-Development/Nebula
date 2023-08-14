43769783719c1624e14db0dc843ccdb520cf02ad
NotIntense <mbro8210@gmail.com>
2023-08-13 20:53:53 -0400

    scp 079 losing signal event

--- NeBuli/Events/Handlers/Scp079Handlers.cs
+++ NeBuli/Events/Handlers/Scp079Handlers.cs
@@ -6,5 +6,9 @@ public static class Scp079Handlers
 {
     public static event EventManager.CustomEventHandler<Scp079PingingEvent> PingingEvent;
 
+    public static event EventManager.CustomEventHandler<Scp079LosingSignalEvent> LosingSignalEvent;
+
     internal static void OnScp079Pinging(Scp079PingingEvent ev) => PingingEvent.CallEvent(ev);
+
+    internal static void OnScp079LosingSignal(Scp079LosingSignalEvent ev) => LosingSignalEvent.CallEvent(ev);
 }
