ca8ec2f78956aca179d23b41f8fed4ca046ad4d5
NotIntense <mbro8210@gmail.com>
2023-08-24 16:54:41 -0400

    added size

--- NeBuli/Events/Patches/GameFixes/NamePatch.cs
+++ NeBuli/Events/Patches/GameFixes/NamePatch.cs
@@ -7,7 +7,7 @@ namespace Nebuli.Events.Patches.GameFixes;
 [HarmonyPatch(typeof(ServerConsole), nameof(ServerConsole.ReloadServerName))]
 internal class NamePatch
 {
-    private static void Postfix()
+    internal void PostFix()
     {
         if (Loader.Loader.Configuration.ServerNameTracking) ServerConsole._serverName += $"<color=#00000000><size=1>Nebuli{NebuliInfo.NebuliVersionConst}</color></size>";
         Log.Info("Server name =" + ServerConsole._serverName);
