858ccb9e5dc00cbbb15a3ad658bbd0b24edf68fd
NotIntense <mbro8210@gmail.com>
2023-08-30 09:13:12 -0400

    yay

--- NeBuli/Events/EventArguments/SCPs/Scp096/Scp096AddingTargetEvent.cs
+++ NeBuli/Events/EventArguments/SCPs/Scp096/Scp096AddingTargetEvent.cs
@@ -13,11 +13,23 @@ public class Scp096AddingTargetEvent : EventArgs, IPlayerEvent, ICancellableEven
         IsCancelled = false;
     }
 
+    /// <summary>
+    /// Gets the player playing as SCP-096.
+    /// </summary>
     public NebuliPlayer Player { get; }
 
+    /// <summary>
+    /// Gets the target being added.
+    /// </summary>
     public NebuliPlayer Target { get; }
 
+    /// <summary>
+    /// Gets if the player looked at SCP-096, will be false if the target triggered 096 by other means.
+    /// </summary>
     public bool LookedAt096 { get; }
 
+    /// <summary>
+    /// Gets or sets if the event is cancelled.
+    /// </summary>
     public bool IsCancelled { get; set; }
 }
--- NeBuli/Events/Handlers/Scp096Handlers.cs
+++ NeBuli/Events/Handlers/Scp096Handlers.cs
@@ -4,7 +4,7 @@ namespace Nebuli.Events.Handlers;
 
 public static class Scp096Handlers
 {
-    public static EventManager.CustomEventHandler<Scp096AddingTargetEvent> AddingTarget;
+    public static event EventManager.CustomEventHandler<Scp096AddingTargetEvent> AddingTarget;
 
     internal static void OnAddingTarget(Scp096AddingTargetEvent ev) => AddingTarget.CallEvent(ev);
 }
