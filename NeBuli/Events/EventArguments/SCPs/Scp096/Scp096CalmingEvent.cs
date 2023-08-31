727b3e97492aa19aa690b6ddab17a7ce15449999
NotIntense <mbro8210@gmail.com>
2023-08-30 21:20:58 -0400

    Fix missing XML comments

--- NeBuli/Events/EventArguments/SCPs/Scp096/Scp096CalmingEvent.cs
+++ NeBuli/Events/EventArguments/SCPs/Scp096/Scp096CalmingEvent.cs
@@ -11,9 +11,18 @@ public class Scp096CalmingEvent : EventArgs, IPlayerEvent, ICancellableEvent
         IsCancelled = false;
     }
 
+    /// <summary>
+    /// Gets the player triggering the event.
+    /// </summary>
     public NebuliPlayer Player { get; }
 
+    /// <summary>
+    /// Gets or sets if the remaining time will be cleared or not.
+    /// </summary>
     public bool ClearingTime { get; set; }
 
+    /// <summary>
+    /// Gets or sets if the event is cancelled.
+    /// </summary>
     public bool IsCancelled { get; set; }
 }
