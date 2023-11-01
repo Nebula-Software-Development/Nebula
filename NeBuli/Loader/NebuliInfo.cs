179c7181fee93dd69b8b0df430b1e7371f809043
NotIntense <mbro8210@gmail.com>
2023-11-01 16:57:51 -0400

    updooooooooot :3

--- NeBuli/Loader/NebuliInfo.cs
+++ NeBuli/Loader/NebuliInfo.cs
@@ -4,7 +4,7 @@ namespace Nebuli.Loader;
 
 public static class NebuliInfo
 {
-    internal const string NebuliVersionConst = "1.3.2";
+    internal const string NebuliVersionConst = "1.3.3";
 
     public static Version NebuliVersion => new(NebuliVersionConst);
 }
